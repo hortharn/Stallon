71835f2dee0e89afcdcfff3c2727c293404aa42b
Hortharn <hortharn1st@gmail.com>
2025-08-28 23:30:44 +0100

    Refactor script loading in HTML files for improved organization and readability

--- assets/mobirise/script/modal-loader.js
+++ assets/mobirise/script/modal-loader.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.text())
         .then(html => {
             // Insert modals before closing body tag
+            //document.getElementById('footer03-a').insertAdjacentHTML('afterend', html);
             document.body.insertAdjacentHTML('beforeend', html);
         })
         .catch(error => console.error('Error loading modals:', error));
